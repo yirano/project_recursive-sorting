f3b7ffdc8904f43bdbc2248eb75958dc9177874d
yirano <yirano@protonmail.ch>
2020-09-28 22:29:37 -0400

    Remove zombie codes from searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,20 +1,10 @@
-# TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
-    print('>>>>>>>> start index: ', start)
-    # print('>>>>>>>> middle index: ', middle_ind)
-    print('>>>>>>>> end index: ', end)
-    # print('>>>>>>>> value: ', arr[middle_ind])
-
-    print('-----------------')
     found = False
 
     while end >= start and found is False:
         middle_ind = (start + end) // 2
         if target == arr[middle_ind]:
             found = True
-            print('>>>>>>>> FOUND!!!: ', found)
-            print('>>>>>>>> middle index: ', middle_ind)
             return middle_ind
         else:
             if target <= arr[middle_ind]:
